b17ebae76755cb0d915c86cf3f59fa8272edd901
meghana1997-star <79625389+meghana1997-star@users.noreply.github.com>
2021-11-01 09:31:44 +0530

    123

---
+++ src/db/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/bloggingapp',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log('connection successful');
+    }).catch((error) => {
+        console.log('something wrong', error);
+    })
